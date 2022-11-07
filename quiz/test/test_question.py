e4a998e4e9f21685b421c4bac639c261c0b4aeef
Francesco Vigni <vignif@gmail.com>
2022-11-07 18:15:29 +0100

    test for the questions

---
+++ quiz/test/test_question.py
@@ -0,0 +1,25 @@
+import pandas as pd
+
+import sys
+import os
+
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+import unittest
+from script.fs_utils import QuizQuestion, load_quiz_questions
+import logging
+
+log = logging.getLogger(__name__)
+
+
+class TestQuestions(unittest.TestCase):
+    def setUp(self):
+        log.debug("creating common resources")
+
+    def tearDown(self):
+        log.debug("tearing down common resources")
+
+    def test_hinted_question_has_length_2(self):
+        q = load_quiz_questions()
+        hinted = q[0].get_hinted()
+        self.assertTrue(len(hinted) == 2)
+
