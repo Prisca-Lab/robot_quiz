fa87771a424d31f07e93bc167e62e494c955a6f5
Francesco Vigni <vignif@gmail.com>
2022-11-07 18:14:41 +0100

    new states

--- quiz/script/states.py
+++ quiz/script/states.py
@@ -8,109 +8,111 @@ from time import sleep  # Handle time
 from std_msgs.msg import String
 
 
-"""
-userdata.response_type:
-    'init' - Initial state responded
-    'answer' - User answered a question
-    'not_answer' - User did not answer a question (timeout) try 2 times repeat the same question then move on
-    'hint' - User asked for a hint (remove 2 options from the question)
-"""
-
-
+TIME_OUT = 2
+STATE_INIT_SLEEP = 1
 class Initial(State):
+    """Read Initial story and initiate the quiz"""
     def __init__(self):
         State.__init__(self, outcomes=['start'],  input_keys=['data_in'],
-                       output_keys=['data_out', 'response_type'])
+                       output_keys=['data_out'])
 
     def execute(self, userdata):
         pub = rospy.Publisher('robot_initial_speech', String, queue_size=10)
-        sec = 2
-        rospy.loginfo(f'In {self.__class__.__name__} state for {sec} seconds')
-        sleep(sec)
-        # rospy.loginfo(userdata.data_in)
-        userdata.response_type = 'init'
+        rospy.loginfo(
+            f'In {self.__class__.__name__} state for {STATE_INIT_SLEEP} seconds')
+        sleep(STATE_INIT_SLEEP)
         pub.publish(userdata.data_in['story'])
-        # Robot reads the Initial greeting sentence in the experiment
         return 'start'
+
+
+class Quiz(State):
+    def __init__(self):
+        State.__init__(self, outcomes=["question", "finish"], input_keys=[
+            "data_in"], output_keys=["data_out"])
+
+    def execute(self, userdata):
+        rospy.loginfo(
+            f'In {self.__class__.__name__} state for {STATE_INIT_SLEEP} seconds')
+        sleep(STATE_INIT_SLEEP)
+        data_dict_out = userdata.data_in
+
+
+        userdata.data_out = data_dict_out
+
+
+class RobotSpeak(State):
+    def __init__(self):
+        State.__init__(self, outcomes=["to_human"], input_keys=[
+            "data_in"], output_keys=["data_out"])
+
+    def execute(self, userdata):
+        rospy.loginfo(
+            f'In {self.__class__.__name__} state for {STATE_INIT_SLEEP} seconds')
+        sleep(STATE_INIT_SLEEP)
+        data_dict_out = userdata.data_in
+
+
+        userdata.data_out = data_dict_out
+
 
 
 class HumanTurn(State):
     def __init__(self):
-        State.__init__(self, outcomes=['respond_to_robot'], input_keys=[
-                       'data_in'], output_keys=['data_out', 'response_type'])
+        State.__init__(self, outcomes=["answer", "no_answer", "request_hint"], input_keys=[
+            "data_in"], output_keys=["data_out"])
 
     def execute(self, userdata):
-        sec = 5
-        rospy.loginfo(f'In {self.__class__.__name__} state for {sec} seconds')
-        sleep(sec)
-
+        rospy.loginfo(
+            f'In {self.__class__.__name__} state for {STATE_INIT_SLEEP} seconds')
+        sleep(STATE_INIT_SLEEP)
         data_dict_out = userdata.data_in
-        data_dict_out['current_question'] = userdata.data_in['current_question']
-        try:
-            timeout = 10
-            userdata.response_type = 'answer'
-            rospy.loginfo(f'Waiting {timeout} seconds for user response')
-
-            data_dict_out['response'] = rospy.wait_for_message(
-                'human_response', String, timeout=timeout).data
-            # rospy.loginfo(userdata.data_out['response'])
-            # msg = rospy.wait_for_message('response', String, timeout=10)
-        except rospy.ROSException as e:
-            rospy.logerr(e)
-            userdata.response_type = 'not_answer'
-            return 'respond_to_robot'
 
         userdata.data_out = data_dict_out
-        return 'respond_to_robot'
 
 
-class RobotTurn(State):
+class RepeatTurn(State):
     def __init__(self):
-        State.__init__(self, outcomes=['question', 'finish'], input_keys=[
-                       'data_in', 'response_type'], output_keys=['data_out'])
-        self.count_not_answer = 0
-        self.pub = rospy.Publisher('say', String, queue_size=10)
+        State.__init__(self, outcomes=["question", "next_question"], input_keys=[
+            "data_in"], output_keys=["data_out"])
 
     def execute(self, userdata):
-        sec = 5
-        rospy.loginfo(f'In {self.__class__.__name__} state for {sec} seconds')
-        sleep(sec)
-
-        data_dict_out = {}
+        rospy.loginfo(
+            f'In {self.__class__.__name__} state for {STATE_INIT_SLEEP} seconds')
+        sleep(STATE_INIT_SLEEP)
         data_dict_out = userdata.data_in
 
-        if userdata.response_type == 'answer':
-            current_question = userdata.data_in['current_question']
-            current_question.check(userdata.data_in['response'])
+        userdata.data_out = data_dict_out
 
-        elif userdata.response_type == 'not_answer':
-            self.count_not_answer += 1
-            if self.count_not_answer == 2:
-                current_question.done == True
-            print("User did not answer, what to do?")
-        
-        elif userdata.response_type == 'hint':
-            print("User asked for a hint!")
 
-        for q in userdata.data_in['quiz_questions']:
-            if q.done == False:
-                rospy.loginfo(q.question)
-                self.pub.publish(q.question)
-                data_dict_out['current_question'] = q
-                userdata.data_out = data_dict_out
-                self.count_not_answer = 0
-                return 'question'
-        
-        rospy.loginfo('All questions done')
-        return 'finish'
+class Hint(State):
+    def __init__(self):
+        State.__init__(self, outcomes=["question"], input_keys=[
+            "data_in"], output_keys=["data_out"])
 
-        # # userdata.data_in['quiz_questions']
+    def execute(self, userdata):
+        rospy.loginfo(
+            f'In {self.__class__.__name__} state for {STATE_INIT_SLEEP} seconds')
+        sleep(STATE_INIT_SLEEP)
+        data_dict_out = userdata.data_in
+        question = data_dict_out['current_question']
+        hinted_question = question.get_hinted()
 
-        # self.pub.publish('I am the robot and i am talking')
-        # if self.c <=10:
-        #     return 'question'
-        # else:
-        #     return 'finish'
+        userdata.data_out = data_dict_out
+
+
+
+class CheckAnswer(State):
+    def __init__(self):
+        State.__init__(self, outcomes=["next_question"], input_keys=[
+            "data_in"], output_keys=["data_out"])
+
+    def execute(self, userdata):
+        rospy.loginfo(
+            f'In {self.__class__.__name__} state for {STATE_INIT_SLEEP} seconds')
+        sleep(STATE_INIT_SLEEP)
+        data_dict_out = userdata.data_in
+
+        userdata.data_out = data_dict_out
 
 
 class Final(State):
@@ -119,13 +121,15 @@ class Final(State):
                        'data_in'], output_keys=['data_out'])
 
     def execute(self, userdata):
-        sec = 2
-        rospy.loginfo(f'In {self.__class__.__name__} state for {sec} seconds')
-        sleep(sec)
-        # rospy.loginfo('The user entered: ' + str(userdata.data_in))
-        return 'succeeded'
+        rospy.loginfo(
+            f'In {self.__class__.__name__} state for {STATE_INIT_SLEEP} seconds')
+        sleep(STATE_INIT_SLEEP)
+        data_dict_out = userdata.data_in
 
-# Main method
+        userdata.data_out = data_dict_out
+
+
+        return 'succeeded'
 
 
 def main():
@@ -143,14 +147,31 @@ def main():
     # Open the state machine container. A state machine container holds a number of states.
     with sm:
 
-        StateMachine.add('Initial', Initial(), transitions={'start': 'RobotTurn'}, remapping={'data_in': 'sm_input',
-                                                                                              'data_out': 'sm_input'})
-        StateMachine.add('RobotTurn', RobotTurn(), transitions={
-                         'question': 'HumanTurn', 'finish': 'Final'}, remapping={'data_in': 'sm_input',
-                                                                                 'data_out': 'sm_input'})
+        StateMachine.add('Initial', Initial(), transitions={'start': 'Quiz'}, remapping={'data_in': 'sm_input',
+                                                                                         'data_out': 'sm_input'})
+        StateMachine.add('Quiz', Quiz(), transitions={
+                         'question': 'RobotSpeak', 'finish': 'Final'}, remapping={'data_in': 'sm_input',
+                                                                                  'data_out': 'sm_input'})
+
+        StateMachine.add('RobotSpeak', RobotSpeak(), transitions={'to_human': 'HumanTurn'}, remapping={'data_in': 'sm_input',
+                                                                                                       'data_out': 'sm_input'})
+
         StateMachine.add('HumanTurn', HumanTurn(), transitions={
-                         'respond_to_robot': 'RobotTurn'}, remapping={'data_in': 'sm_input',
-                                                                      'data_out': 'sm_input'})
+                         'answer': 'CheckAnswer', 'no_answer': 'RepeatTurn', 'request_hint': 'Hint'}, remapping={'data_in': 'sm_input',
+                                                                                                                 'data_out': 'sm_input'})
+
+        StateMachine.add('RepeatTurn', RepeatTurn(), transitions={
+                         'question': 'RobotSpeak', 'next_question': 'Quiz'}, remapping={'data_in': 'sm_input',
+                                                                                        'data_out': 'sm_input'})
+
+        StateMachine.add('Hint', Hint(), transitions={
+                         'question': 'RobotSpeak'}, remapping={'data_in': 'sm_input',
+                                                               'data_out': 'sm_input'})
+
+        StateMachine.add('CheckAnswer', CheckAnswer(), transitions={
+                         'next_question': 'Quiz'}, remapping={'data_in': 'sm_input',
+                                                              'data_out': 'sm_input'})
+
         StateMachine.add('Final', Final(), transitions={
                          'succeeded': 'succeeded'}, remapping={'data_in': 'sm_input',
                                                                'data_out': 'sm_input'})
