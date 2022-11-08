a86d992773e0d2e46fb2d827724fd5d61ae0768e
francesco <vignif@gmail.com>
2022-11-09 00:42:27 +0100

    implement srv

---
+++ picovoice_ros/src/pico_node.py
@@ -0,0 +1,150 @@
+#!/usr/bin/env python
+
+import sys
+import os
+
+
+from picovoice_ros.srv import AskUser, AskUserResponse
+
+import pvrhino
+from pvrecorder import PvRecorder
+import wave
+import struct
+import time
+
+from pathlib import Path
+import rospy
+import logging
+log = logging.getLogger(__name__)
+
+
+MODEL = os.path.join(os.path.dirname(os.path.dirname(
+    os.path.realpath(__file__))), 'picovoice/rhino/', 'rhino_params_it.pv')
+CONTEXT = os.path.join(os.path.dirname(os.path.dirname(
+    os.path.realpath(__file__))), 'picovoice/rhino/', 'quiz_it_linux_v2_1_0.rhn')
+
+
+class IntentRecognition:
+
+    def __init__(
+            self,
+            access_key="30uXi1g4+23OWdquCouuIax2CfKWmTzfvC9IFkSrYmhKTAapX1q4Zg==",
+            library_path=pvrhino.LIBRARY_PATH,
+            model_path=MODEL,
+            context_path=CONTEXT,
+            endpoint_duration_sec=1.0,
+            require_endpoint=True,
+            audio_device_index=-1,
+            output_path=None):
+
+        self._access_key = access_key
+        self._library_path = library_path
+        self._model_path = model_path
+        self._context_path = context_path
+        self._endpoint_duration_sec = endpoint_duration_sec
+        self._require_endpoint = require_endpoint
+        self._audio_device_index = audio_device_index
+        self._output_path = output_path
+        try:
+            self.rhino = pvrhino.create(
+                access_key=self._access_key,
+                library_path=self._library_path,
+                model_path=self._model_path,
+                context_path=self._context_path,
+                endpoint_duration_sec=self._endpoint_duration_sec,
+                require_endpoint=self._require_endpoint)
+        except pvrhino.RhinoInvalidArgumentError as e:
+            args = (
+                self._access_key,
+                self._library_path,
+                self._model_path,
+                self._context_path,
+                self._require_endpoint
+            )
+            print("One or more arguments provided to Rhino is invalid: ", args)
+            print("If all other arguments seem valid, ensure that '%s' is a valid AccessKey" %
+                  self._access_key)
+            raise e
+        self.intent = None
+        print("Intent started")
+
+    def run(self, max_seconds=10):
+        """
+         Creates an input audio stream, instantiates an instance of Rhino object, and infers the intent from spoken
+         commands.
+         """
+
+        # self.rhino = None
+        # self.recorder = None
+
+        self.recorder = PvRecorder(
+            device_index=self._audio_device_index, frame_length=self.rhino.frame_length)
+        self.recorder.start()
+
+        self.wav_file = None
+
+        try:
+            if self._output_path is not None:
+                self.wav_file = wave.open(self._output_path, "w")
+                self.wav_file.setparams((1, 2, 16000, 512, "NONE", "NONE"))
+
+            print(self.rhino.context_info)
+            print()
+
+            print("Using device: %s" % self.recorder.selected_device)
+            print("Listening...")
+            print()
+            start = time.time()
+            while time.time() - start < max_seconds:
+                pcm = self.recorder.read()
+
+                if self.wav_file is not None:
+                    self.wav_file.writeframes(struct.pack("h" * len(pcm), *pcm))
+
+                is_finalized = self.rhino.process(pcm)
+                if is_finalized:
+                    inference = self.rhino.get_inference()
+                    if inference.is_understood:
+                        print('{')
+                        print("  intent : '%s'" % inference.intent)
+                        print('  slots : {')
+                        for slot, value in inference.slots.items():
+                            print("    %s : '%s'" % (slot, value))
+                        print('  }')
+                        print('}\n')
+                        self.intent= inference.intent
+                        self.recorder.delete()
+                        return self.intent
+                    else:
+                        self.intent = None
+                        print("Didn't understand the command.\n")
+        except Exception as e:
+            rospy.logerr(e)
+
+    def stop(self):
+        if self.recorder is not None:
+            self.recorder.delete()
+
+        if self.rhino is not None:
+            self.rhino.delete()
+
+        if self.wav_file is not None:
+            self.wav_file.close()
+
+
+class PicoNode:
+    def __init__(self) -> None:
+        self.service = rospy.Service('ask_user', AskUser, self.handle_user)
+        self.intent_recognition = IntentRecognition()
+
+    def handle_user(self, req):
+        # print("Returning [%s + %s = %s]"%(req.a, req.b, (req.a + req.b)))
+        val = self.intent_recognition.run()
+        print(val)
+        return AskUserResponse(val)
+
+
+if __name__ == '__main__':
+    rospy.init_node('pico_node')
+    P = PicoNode()
+    rospy.spin()
