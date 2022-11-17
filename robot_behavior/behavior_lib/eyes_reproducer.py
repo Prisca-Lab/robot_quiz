f7faffd6d274f1f50f4c4d0375df91fd4c583360
Francesco Vigni <vignif@gmail.com>
2022-11-17 11:20:01 +0100

    cleanup

--- robot_behavior/behavior_lib/eyes_reproducer.py
+++ robot_behavior/behavior_lib/eyes_reproducer.py
@@ -4,23 +4,24 @@ from hri_msgs.msg import Expression
 from behavior_lib.abstract_behaviour import BehaviourMode
 import time
 
+
 class Eyes(BehaviourMode):
 
     def __init__(self):
         name = self.__class__.__name__
         super().__init__(name)
-        
+
         self.eyes_pub = rospy.Publisher(
             '/eyes/expression', Expression, queue_size=10)
 
     # define publisher for changing robot facial expression
     def execute(self):
-        if self.data is not None:            
-            print(f"executing {self.name} at {time.time()}")
+        if self.data is not None:
             expression_msg = Expression()
             # expression_msg.header = std_msgs.msg.Header() #uncomment for version > 0.7.0
             expression_msg.expression = self.data
             rospy.loginfo(expression_msg)
+            print(f"executing {self.name} at {time.time()}")
             self.eyes_pub.publish(expression_msg)
         else:
             rospy.logerr("Please set data before executing")
